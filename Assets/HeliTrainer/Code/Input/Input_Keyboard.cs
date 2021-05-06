3d4b8dc972d5bd34c4969b9ca5c124425487d418
Bernard Scheuter <31876955+BernardScheuter@users.noreply.github.com>
2021-05-06 11:13:44 +0200

    Add project files.

---
+++ Assets/HeliTrainer/Code/Input/Input_Keyboard.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CantThinkOfAName
+{
+    public class Input_Keyboard : Input_base
+    {
+        #region Variables
+        private float throttleInput = 0f;
+        private float colletiveInput = 0f;
+        private float cyclicInput = 0f;
+        private float pedalInput = 0f;
+        #endregion
+        
+        #region Properties
+        public float ThrottleInput
+        { get {return throttleInput;} }
+        public float CollectiveInput
+        { get {return colletiveInput; }}
+        public float CyclicInput
+        { get { return cyclicInput; } }
+        public float PedalInput
+        { get { return pedalInput; } }
+        #endregion
+
+        #region Base Methods
+        #endregion
+
+        #region Custom Methods
+        protected override void HandleInputs()
+        {
+            base.HandleInputs();
+            throttleInput = VerticalInput;
+            pedalInput = HorizontalInput;
+        }
+        #endregion
+    }
+}
---
+++ Assets/HeliTrainer/Code/Input/testScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CantThinkOfAName;
+
+    public class testScript : MonoBehaviour
+    {
+        public Input_Keyboard input;
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        float throttleInput = input.ThrottleInput;
+        Debug.Log(throttleInput);
+        }
+    }
